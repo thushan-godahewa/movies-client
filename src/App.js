4ef86da77818b3a28a6c51df68aefb076be8e338
Thushan Godahewa <thushan.godahewa@gmail.com>
2024-04-03 22:57:08 +1100

    Updated to add carousel feature

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { useState, useEffect } from 'react';
 import Layout from './components/Layout';
 import Home from './components/home/Home';
 import { Routes, Route } from 'react-router-dom';
+import Header from './components/header/Header';
 
 function App(){
 
@@ -25,6 +26,7 @@ function App(){
 
   return (
     <div className='App'>
+      <Header/>
       <Routes>
         <Route path="/" element={<Layout/>}>
           <Route path="/" element={<Home movies={movies}/>}>
--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -12,7 +12,7 @@ const Hero = ({movies}) => {
                 return(
                     <Paper key={movie.imdbId}>
                         <div className = 'movie-card-container'>
-                            <div className="movie-card" >
+                            <div className="movie-card" style={{"--img": `url(${movie.backdrops[0]})`}}>
                                 <div className="movie-detail">
                                     <div className="movie-poster">
                                         <img src={movie.poster} alt="" />
